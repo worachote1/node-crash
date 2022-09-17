aa9442f049c8a35ae2ed2bdf8ecf9d5fda112651
worachote1 <timeworachote2002@gmail.com>
2022-09-18 03:09:05 +0700

    Play with MongoDb

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const express = require('express')
-
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 //express app
 const app = express()
-
+//connect to mongoDB
+const dbURI ='mongodb+srv://worachote:232903@cluster0.r5ufrlg.mongodb.net/node-tuts?retryWrites=true&w=majority'
+mongoose.connect(dbURI,{useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result)=>{
+        console.log("connect to db")
+        //listen for request aftermconection to database is complete
+        app.listen(3000)
+    }
+    )
+    .catch((err)=>{console.log(err)})
 //register view engine
 app.set('view engine','ejs')
 
+//middleware and static files
+app.use(express.static('public'))
+app.use(morgan('dev'))
 
-//listen for request
-app.listen(3000)
+//routes
 app.get('/',(req,res)=>{
-    //res.send('<p>home page</p>');
-    const blogs = [
-        {title:"Yoshi finds eggs",snippet:'lorem 124'},
-        {title:"Mario finds stars",snippet:'lorem 124'},
-        {title:"How to binsaig",snippet:'lorem 124'}
-    ]
-    res.render('index',{
-        title : "44Home" , AllBlogs : blogs
-    })
+    res.redirect('/blog')
 });
 app.get('/about',(req,res)=>{
     //res.send('<p>about page</p>');
@@ -26,6 +31,19 @@ app.get('/about',(req,res)=>{
         title : "44About"
     })
 });
+
+//blog routes
+app.get("/blog",(req,res)=>{
+    //find all document inside Blog collection
+    //and sort the Newest blog in the first
+    Blog.find().sort({createdAt : -1})
+    .then((result)=>{
+        res.render('index',{title: "All Blog 44prn",AllBlogs: result })
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+})
 
 app.get('/blogs/create',(req,res)=>{
     res.render('create',{
